16b9fad812110e94d5ef42076389f42bfcc2b7f9
Kohei Onodera <onodera@iact.co.jp>
2014-04-29 20:28:30 +0900

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: iptables-enable
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+iptables_rule	"http"
+iptables_rule	"ssh"
+
+
